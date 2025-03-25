dd924b4f5855f85d26e6bcea0bc9d18c2b1c8a2b
Dimas Wisnu Wirawan <101412302+dimaswi@users.noreply.github.com>
2025-03-26 02:39:26 +0700

    Update ComboBox.jsx

--- resources/js/Components/ComboBox.jsx
+++ resources/js/Components/ComboBox.jsx
@@ -1,4 +1,4 @@
-import { Button } from '@/components/ui/button';
+import { Button } from './ui/button';
 import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from '@/components/ui/command';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 import { cn } from '@/lib/utils';
